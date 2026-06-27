a68dafec810827751647e456b4fb6c69e7cd48e8
YukariYuri <tonnamhttp@gmail.com>
2026-06-27 21:07:53 +0700

    ปรับโค้ดให้กันได้ทั้ง FE และ BE

--- API.js
+++ API.js
@@ -3,7 +3,13 @@ const ApiPort = 'http://localhost:3000'
 async function BmiCal() {
     const weight = document.getElementById("weight").value;
     const height = document.getElementById("height").value;
+
+    if (isNaN(Number(weight)) || isNaN(Number(height)) || weight === "" || height === "") 
+        return alert("Number invalid or exceeds limit.");
     
+    if (weight <= 0 || height <= 0) 
+        return alert("Please fill in your weight and height information correctly. (Value must be greater than 0)")
+
     const response = await fetch(`${ApiPort}/BmiCal?weight=${weight}&height=${height}`)
         .then(res => res.json())
 
